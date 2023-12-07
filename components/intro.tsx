180d6a9fe35cebec09912449160c6698d06af431
Renan <108462665+Lafayette112@users.noreply.github.com>
2023-12-07 14:09:19 +0100

    Update Linkedin link

--- components/intro.tsx
+++ components/intro.tsx
@@ -83,7 +83,7 @@ export default function Intro() {
                 </a>
 
                 <a 
-                    href="https://www.linkedin.com/in/renan-kouadio-a58a7b273"
+                    href="https://www.linkedin.com/in/renan-kouadio"
                     target='_blank'
                     className='bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60'
                 >
